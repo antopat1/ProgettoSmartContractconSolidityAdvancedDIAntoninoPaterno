81d15d46fdc1a790e158e93cdd29924bce469a91
antopat1 <antopat1@gmail.com>
2024-11-05 20:27:36 +0100

    UpdateHardHatconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,3 @@
-// import type { HardhatUserConfig } from "hardhat/config";
-// import "@nomicfoundation/hardhat-toolbox-viem";
-
-// const config: HardhatUserConfig = {
-//   solidity: "0.8.27",
-// };
-
-// export default config;
-
-
-// import { HardhatUserConfig } from "hardhat/config";
-// import "@nomicfoundation/hardhat-toolbox-viem";
-
-// const config: HardhatUserConfig = {
-//   solidity: {
-//     version: "0.8.9",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-//   networks: {
-//     hardhat: {
-//       mining: {
-//         auto: true,
-//         interval: 0
-//       }
-//     }
-//   }
-// };
-
-// export default config;
-
-
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
